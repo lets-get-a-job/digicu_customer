499100198ef0091a54dd359a2d06fbe85eacd945
정지승 <49086747+JungJiSeung@users.noreply.github.com>
2021-04-28 14:34:49 +0900

    SignUp Activity layout design

---
+++ app/src/main/java/com/example/digicu_customer/ui/login/signup/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.digicu_customer.ui.login.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.digicu_customer.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
